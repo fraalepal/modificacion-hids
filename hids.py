da76eec85311b1eaaaafe1bdc1df4c64d17914a4
Marcel Dan <gigi.dan2011@gmail.com>
2020-10-26 12:33:21 +0100

    Añadida función que parsea el dir y hash desde archivo
    Añadida función que calcula los hashes cada cierto tiempo y las guarda
    en un diccionario
    
    Añadida función que compara entre los hashes calculados por la funcíón
    anterior con los del archivo 'hashes.hash'
    
    En la variable 'newFilesAndHashes' es donde se guardan los hashes
    importados del archivo 'hashes.hash'

--- hids.py
+++ hids.py
@@ -4,6 +4,7 @@ import os
 # GLOBALS
 configDict = dict()
 filesAndHashes = dict()
+newFilesAndHashes = dict()
 
 
 def folderHash(pathName):
@@ -23,17 +24,21 @@ def importConfig():
         try:
             with open("config.config", "r") as config:
                 for line in config:
-                    confSplitted = line.split("=")
-                    configDict[confSplitted[0].strip(
-                    )] = confSplitted[1].strip()
+                    if "#" not in line:
+                        confSplitted = line.split("=")
+                        configDict[confSplitted[0].strip(
+                        )] = confSplitted[1].strip()
             print("¡La configuración se ha cargado correctamente!")
-            print(configDict)
+            # print(configDict)
         except:
             print("¡No se ha podido cargar la configuracion, revisa la sintaxis!")
     else:
-        configs = ["Selected Hash=\n", "Directories to protect=\n"]
+        configs = ["Selected Hash=\n",
+                   "Directories to protect=\n", "Verify interval=\n"]
         try:
             with open("config.config", "w") as file:
+                file.write(
+                    "# To list directories, write them separated by comma\n# Interval time in minutes")
                 for config in configs:
                     file.write(config)
             print("¡Archivo de configuración creado!")
@@ -52,10 +57,44 @@ def exportHashedFiles():
             writer.write(key + "=" + value + "\n")
 
 
-# def loadHashedFiles():
-# def checkIntegrity():
-"""folderHash(
-    "C:/Users/Marcel/OneDrive - UNIVERSIDAD DE SEVILLA/GitHub/carpetaEjemplo")
-"""
+def importHashedFiles():
+    with open("hashes.hash", "r") as reader:
+        line = reader.readline()
+        while line:
+            splittedLineList = line.split("=")
+            newFilesAndHashes[splittedLineList[0].replace(
+                "\n", "")] = splittedLineList[1].replace("\n", "")
+            line = reader.readline()
+    # print(newFilesAndHashes)
+
+
+def calculateHashedFiles():
+    splittedPathsToHash = configDict["Directories to protect"].split(
+        ",")  # para ser mejor, hacer strip con un for para cada elemento por si acaso
+    for path in splittedPathsToHash:
+        filesAndHashes.update(folderHash(path))
+
+
+def compareHashes():
+    numberOfFilesOK = int()
+    numberOfFilesNoOk = int()
+    listOfNoMatches = list()
+    for key, value in filesAndHashes.items():
+        if newFilesAndHashes[key] == value:
+            numberOfFilesOK += 1
+        else:
+            numberOfFilesNoOk += 1
+            cadena = "DIR: " + str(key) + " HASHES DOESN'T MATCH!"
+            listOfNoMatches.append(cadena)
+    print("\nNumber of files OK: " + str(numberOfFilesOK))
+    print("Number of files BAD: " + str(numberOfFilesNoOk))
+    print("BAD integrity files: ")
+    print('\n '.join(listOfNoMatches))
+
+
 importConfig()
-exportHashedFiles()
+importHashedFiles()
+calculateHashedFiles()
+compareHashes()
+
+# exportHashedFiles()
